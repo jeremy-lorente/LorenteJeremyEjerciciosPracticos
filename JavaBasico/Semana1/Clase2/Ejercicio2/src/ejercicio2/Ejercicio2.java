9f13b14656d29a6e892b0d473b7d5496ca94dff1
Jeremy <jeremylorente98@gmail.com>
2023-12-13 18:39:56 +0100

    modificado ejercicio 2 clase 2 para usar bandera

--- JavaBasico/Semana1/Clase2/Ejercicio2/src/ejercicio2/Ejercicio2.java
+++ JavaBasico/Semana1/Clase2/Ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -7,12 +7,13 @@ public class Ejercicio2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double total = 0;
-        while (true) {
+        boolean bandera = true;
+        while (bandera) {
             System.out.println("Introduce el precio del producto a sumar (Numerico, 0 para salir del programa)");
             if (sc.hasNextDouble()) {
                 double precio = sc.nextDouble();
                 if (precio <= 0) {
-                    break;
+                    bandera = false;
                 }
                 total += precio;
 
